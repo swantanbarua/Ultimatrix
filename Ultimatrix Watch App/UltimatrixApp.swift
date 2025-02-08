fc7a42f9f2a929303be7cc58f6c0325dfa999256
Swantan Barua <work.swantan@gmail.com>
2025-02-08 13:04:34 +0530

    Initial Commit

---
+++ Ultimatrix Watch App/UltimatrixApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatrixApp.swift
+//  Ultimatrix Watch App
+//
+//  Created by Swantan Barua on 08/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Ultimatrix_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
